25839df00f38646c9499ed41e0db85a53b75621a
Matthew Scheffel <matt@weeoak.com>
2015-12-13 21:56:35 -0500

    Added some comments and docstrings

--- cross-correlate.py
+++ cross-correlate.py
@@ -4,16 +4,28 @@ from math import sqrt
 from csv import reader
 
 def normalization_divisor(set_a, set_b):
+    """ Given two sets, returns the divisor that should be used to 
+        normalize the cross-correlation value """
     a_squared_sum = 0
     b_squared_sum = 0
 
     for i in range(0, len(set_a)):
-        a_squared_sum = a_squared_sum + set_a[i]**2
-        b_squared_sum = b_squared_sum + set_b[i]**2
+        a_squared_sum += set_a[i]**2
+        b_squared_sum += set_b[i]**2
 
     return sqrt(a_squared_sum*b_squared_sum)
 
 def cross_correlate(set_a, set_b, normalize=False, unbaised=False):
+    """ Given two equally-sized sets, returns the correlation set computed
+    by taking a sliding "lag" window that is used to offset the second set.
+    
+    Takes an optional "normalize" flag that will normalize each item in the 
+    correlation set (off by default). Also takes an "unbaised" flag that will
+    weigh each computation of the correlation of the two sets for a given lag, 
+    giving greater lags greater weight. Since the sets have the same length,
+    lag can bias the correlation to lower values because there are fewer data
+    to iterate over, and this technique can help avoid that. It's off by default.
+    """
     correlation_set = {}
     if normalize:
         n_divisor = normalization_divisor(set_a, set_b)
@@ -24,17 +36,17 @@ def cross_correlate(set_a, set_b, normalize=False, unbaised=False):
             c_sum = 0
             for i in range(0, len(set_a)):
                 if i - lag < 0 or i - lag >= len(set_a):
-                    print ".",
+                    print ".", # "miss" - the lag prevents a hit for i in both sets a and b
                 else:
-                    print "!",
+                    print "!", # "hit" - there's a corresponding set_b[i] for this set_a[i]
                     c_sum += set_a[i]*set_b[i-lag]
             print ", = %.2f" % c_sum
         
             if unbaised:
-                c_sum /= len(set_a) - abs(lag)
+                c_sum /= len(set_a) - abs(lag) # greater lag, greater c_sum
             
             if normalize:
-                correlation_set[lag] = c_sum/n_divisor
+                correlation_set[lag] = c_sum/n_divisor # FIXME unbaised flag probably makes this normalization invalid
             else:
                 correlation_set[lag] = c_sum
 
@@ -44,6 +56,7 @@ def cross_correlate(set_a, set_b, normalize=False, unbaised=False):
         return -1
 
 def best_correlation(set_a, set_b, normalize=True, unbaised=False):
+    """ Returns the best correlation found by cross_correlate(), and the lag required to find it. """
     correlation_set = cross_correlate(set_a, set_b, normalize=normalize, unbaised=unbaised)
     maximum_correlation_position = max(correlation_set.iterkeys(), key=(lambda key: correlation_set[key]))
 
@@ -56,7 +69,7 @@ if __name__ == "__main__":
     set_b = []
 
     with open('input.csv', 'rb') as csvfile:
-        inputreader = reader(csvfile)
+        inputreader = reader(csvfile) # header\nindex,set_a,set_b\nindex,set_a,set_b\n...
 
         # skip header
         next(inputreader)
